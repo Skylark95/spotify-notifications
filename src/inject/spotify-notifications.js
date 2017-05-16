359006ff4332939e5821a3dbfd9f55fe8c06043f
Derek Cochran <derek@skylark95.com>
2017-05-15 21:25:56 -0500

    Notify on page load

--- src/inject/spotify-notifications.js
+++ src/inject/spotify-notifications.js
@@ -3,30 +3,16 @@ var spotifyNotifications = {
     Notification.requestPermission().then((result) => {
       if (result === "granted") {
         this.observe();
+        this.notify();
       }
     });
   },
 
   observe() {
+    let observer = new MutationObserver(() => { this.notify(); });
     let observerConfig = { characterData: true, subtree: true };
-    let observer = this.mutationObserver();
     this.querySelector('.now-playing').then(nowPlaying => {
       observer.observe(nowPlaying, observerConfig);
-    });
-  },
-
-  mutationObserver() {
-    return new MutationObserver(() => {
-      Promise.all([
-        this.querySelector('.now-playing .track-info__name'),
-        this.querySelector('.now-playing .track-info__artists'),
-        this.querySelector('.now-playing .cover-art-image-loaded')
-      ]).then(elements => {
-        let name = elements[0].textContent,
-            artists = elements[1].textContent,
-            image = elements[2].style.backgroundImage.slice(5, -2);
-        this.notify(name, artists, image);
-      });
     });
   },
 
@@ -44,7 +30,20 @@ var spotifyNotifications = {
     });
   },
 
-  notify(name, artists, image) {
+  notify() {
+    Promise.all([
+      this.querySelector('.now-playing .track-info__name'),
+      this.querySelector('.now-playing .track-info__artists'),
+      this.querySelector('.now-playing .cover-art-image-loaded')
+    ]).then(elements => {
+      let name = elements[0].textContent,
+          artists = elements[1].textContent,
+          image = elements[2].style.backgroundImage.slice(5, -2);
+      this.doNotify(name, artists, image);
+    });
+  },
+
+  doNotify(name, artists, image) {
     let options = {body: artists, icon: image};
     let n = new Notification(name, options);
     setTimeout(n.close.bind(n), 8000);
