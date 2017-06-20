1b6aa098cc3030fa53324494ea32e0d68ef004df
Derek Cochran <derek@skylark95.com>
2017-06-19 22:27:29 -0500

    Implement browser action

--- src/browser-action/browser-action.js
+++ src/browser-action/browser-action.js
@@ -1,8 +1,13 @@
-var data = {
-  name: 'Spoonman',
-  artists: 'Soundgarden',
-  image: 'https://i.scdn.co/image/4f26396a6ab946e4df08a144372b1b2c60a6cb89'
-};
-document.querySelector('.cover-art-image').style.backgroundImage = `url('${data.image}')`;
-document.querySelector('.track-info-name').innerHTML = data.name;
-document.querySelector('.track-info-artists').innerHTML = data.artists;
+chrome.storage.local.get('spotifyNotifications.tab.id', items => {
+  let tabId = items['spotifyNotifications.tab.id'];
+  chrome.tabs.sendMessage(tabId, {src: "spotifyNotifications.browserAction"}, response => {
+    if (response.data) {
+      document.querySelector('.cover-art-image').style.backgroundImage = `url('${response.data.image}')`;
+      document.querySelector('.track-info-name').innerHTML = response.data.name;
+      document.querySelector('.track-info-artists').innerHTML = response.data.artists;
+    }
+ });
+ document.querySelector('body').addEventListener('click', () => {
+   chrome.tabs.update(tabId, {active: true});
+ });
+});
