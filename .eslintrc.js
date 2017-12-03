99abb47805106790c1794acc30114caa6c3e53b7
Derek Cochran <derek@skylark95.com>
2017-12-03 15:29:35 -0600

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "webextensions": true,
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
